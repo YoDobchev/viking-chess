22b0cbe580c5646dd0ff679344a856504921885d
YoDobchev <yo.dobchev@gmail.com>
2025-01-15 03:28:15 +0200

    Forgot strlen

--- main.cpp
+++ main.cpp
@@ -87,6 +87,14 @@ int strcmp(const char* str1, const char* str2) {
 	return *str1 - *str2;
 }
 
+size_t strlen(const char* str) {
+	size_t length = 0;
+	while (str[length] != '\0') {
+		length++;
+	}
+	return length;
+}
+
 enum PositionTypes {
 	PIECE,
 	SQUARE,
